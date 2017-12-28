86549c847f8ee5e0300b932c3253980cfcb8cf0d
AdrianLayton <AdrianLayton@gmail.com>
2017-12-28 11:10:03 -0500

    Test on AWS

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
+
 // ******** PACKAGES ********
 
 const express = require ('express');
 const bodyParser = require('body-parser');
 const AWS = require("aws-sdk");
+const request = require('superagent');
 
 const app = express();
 
@@ -15,15 +18,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 AWS.config.update({
 	region: "us-east-1"
 });
-AWS.config.dynamodb({
+AWS.config.dynamodb = {
 	endpoint: "dynamodb.us-east-1.amazonaws.com"
-})
+};
 
 
 
 let docClient = new AWS.DynamoDB.DocumentClient();
 
-let table = "testDb";
+
 
 
 let myCredential = AWS.config.getCredentials(function(err) {
@@ -38,6 +41,11 @@ app.get('/', (req,res) => {
 })
 
 app.post('/', (req,res) => {
+
+	mailChimpApiKey = c9d440b116a32a6cd1681156f1a4c46d-us17
+	mailChimpInstance = us17
+	listUniqueId = baa4a9093b
+	const table = "newDB";
 	let firstName = req.body.fname;
 	let lastName = req.body.lname;
 	let email = req.body.email;
@@ -58,6 +66,26 @@ app.post('/', (req,res) => {
 	        console.log("Added item:", JSON.stringify(data, null, 2));
 		}
 })
+	request
+        .post('https://' + mailChimpInstance + '.api.mailChimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + mailChimpApiKey ).toString('base64'))
+        .send({
+          'email_address': req.body.email,
+          'status': 'subscribed',
+          'merge_fields': {
+            'fname': req.body.firstName,
+            'lname': req.body.lastName
+          }
+        })
+            .end(function(err, response) {
+              if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+                res.send('Signed Up!');
+              } else {
+                res.send('Sign Up Failed :(');
+              }
+          });
+    res.sendFile(__dirname + "emailConfirm.html");
 })
 
 
